7cb2b84a1f8eae10cf21aad472e16c13ec9d8ac2
nigelritfeld <nigelritfeld@live.nl>
2021-06-14 14:27:30 +0200

    Corrected get params for the namespace worlds

--- api/public/v1/levels/index.php
+++ api/public/v1/levels/index.php
@@ -4,15 +4,15 @@ require_once '../../../config.php';
 require_once ROOT . '/app/includes/settings.php';
 
 switch (($_GET)) {
-    case isset($_GET['worlds']):
+    case isset($_GET['world_id']):
         try {
-            $world_id = $_GET['worlds'];
+            $world_id = $_GET['world_id'];
             if (is_numeric($world_id)) {
                 header('Content-type: application/json');
-                $data = $db->Select("SELECT * FROM `levels` WHERE `world_id` = :id", ["id" => $_GET['worlds']]);
+                $data = $db->Select("SELECT * FROM `levels` WHERE `world_id` = :id", ["id" => $_GET['world_id']]);
                 json_encode($data);
                 if (empty($data)) {
-                    echo 'No levels found for worlds with id: ' . $_GET['worlds'];
+                    echo 'No levels found for worlds with id: ' . $_GET['world_id'];
                 } else {
                     echo $data;
                 }
